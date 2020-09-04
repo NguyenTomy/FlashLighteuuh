e78e9157353e61cf4f6865d207ade1468bf0bd99
Tomy-sama <tom_nguyen0@hotmail.fr>
2020-09-04 10:23:00 +0200

    flashlight

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.flashlight;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void flashlight(View view) {
-                Camera cam = Camera.open();
-                Camera.Parameters p = cam.getParameters();
-                p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                cam.setParameters(p);
-                cam.startPreview();
+        checkPermission(Manifest.permission.CAMERA,
+                CAMERA_PERMISSION_CODE);
     }
 
+
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission)
+                == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] { permission },
+                    requestCode);
+        } else {
+            Camera cam = Camera.open();
+            Camera.Parameters p = cam.getParameters();
+            p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            cam.setParameters(p);
+            cam.startPreview();
+        }
+    }
 
 
 }
